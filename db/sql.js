56438242207f716ffc7c421d7556fab8ccbf7421
Tero Laitinen <tero.laitinen@iki.fi>
2017-01-27 21:29:50 +0200

    store QueryFiles in a cache to avoid creating multiple instances per path

--- db/sql.js
+++ db/sql.js
@@ -1,12 +1,19 @@
 let pgp = require('pg-promise');
 let cfg = require('../app-config');
 let path = require('path');
+
+let queryFiles = {};
+
 module.exports = dir => file => {
   if (cfg.debug) {
     console.log('sql', dir, file);
   }
-  return new pgp.QueryFile(path.join(__dirname, 'sql', dir, file +'.sql'), {
-    minify: true,
-    debug: cfg.debug
-  });
+  let p = path.join(__dirname, 'sql', dir, file + '.sql');
+  if (!(p in queryFiles)) {
+    queryFiles[p] = new pgp.QueryFile(p, {
+      minify: true,
+      debug: cfg.debug
+    });
+  }
+  return queryFiles[p];
 };
