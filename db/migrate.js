87de4ae830ad7579fb1d4d7a871ea461bd3e6e83
Tero Laitinen <tero.laitinen@iki.fi>
2017-02-03 12:29:18 +0200

    run all available m%03d.sql migration scripts (no current version in JS code)

--- db/migrate.js
+++ db/migrate.js
@@ -1,16 +1,21 @@
 let db = require('./db');
 let sql = require('./sql').migrations;
 let _ = require('lodash');
-const myVersion = 0;
 let jed = require('jed');
 
+let scriptName = v => jed.sprintf('m%03d', v);
+
 module.exports = function() {
+
+  let myVersion = 0;
+  for (;scriptName(myVersion+1) in sql; myVersion++);
+
   return db.tx(t => {
     t.none(sql.always);
     return t.one(sql.getVersion)
       .then(row => {
         return t.batch(_.range(myVersion, row.version)
-            .map(v => t.none(sql[jed.sprintf('%03d', v)])))
+          .map(v => t.none(sql[scriptName(v)])))
           .then(() => {
             t.none(sql.updateVersion, {version:myVersion});
           });
